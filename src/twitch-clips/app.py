abbf8ef1085af3e81cb8b5f4b680c41a7d7f1bf2
timtonix <timtonic@outlook.fr>
2023-05-10 17:20:50 +0200

    Add a function that load a category

--- src/twitch-clips/app.py
+++ src/twitch-clips/app.py
@@ -4,9 +4,24 @@ from selenium.webdriver.common.by import By
 
 
 class TwitchClips:
-    def __init__(self, category: str = None, language: str = "en"):
+    def __init__(self,language: str = "en"):
         self.driver = webdriver.Firefox()
-        self.category = category
         self.language = language
+        self.category = None
+
+    @property
+    def category(self):
+        return self.category
+
+    @category.setter
+    def category(self, value: str):
+        self.category = value.lower()
+
+    def load_browse_page(self):
         self.driver.get("https://www.twitch.tv/directory")
 
+    def load_category_page(self, category: str):
+        category = category.lower()
+        self.driver.get(f"https://www.twitch.tv/directory/game/{category}")
+        if self.driver.find_element(By.XPATH, "//div[@data-a-target='core-error-message']"):
+            raise ValueError(f"{category} does not exist on Twitch")
