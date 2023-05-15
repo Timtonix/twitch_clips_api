b99ea2ad6853c6ea9c348f24782b40b5171ad476
timtonix <timtonic@outlook.fr>
2023-05-15 21:31:45 +0200

    After update, the tests should function now...

--- src/twitch_clips/api.py
+++ src/twitch_clips/api.py
@@ -1,11 +1,13 @@
+import selenium.common.exceptions
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import *
 
 
 class TwitchClips:
     def __init__(self,language: str = "en"):
-        self.driver = webdriver.Firefox()
+        self.driver = webdriver.Chrome()
         self.language = language
         self.category = None
 
@@ -16,10 +18,12 @@ class TwitchClips:
         category = category.lower()
         self.driver.get(f"https://www.twitch.tv/directory/game/{category}")
         # Check if it's a 404 page
-        if self.driver.find_element(By.XPATH, "//div[@data-a-target='core-error-message']"):
-            raise ValueError(f"{category} does not exist on Twitch")
-        else:
-            return True
+        try:
+            self.driver.find_element(By.XPATH, "//div[@data-a-target='core-error-message']")
+            raise ValueError(f"{category} is not a category.")
+        except NoSuchElementException:
+            # The category exist
+            pass
 
 
 if __name__ == "__main__":
--- tests/test_twitch_clips.py
+++ tests/test_twitch_clips.py
@@ -2,5 +2,5 @@ import pytest
 import sys
 import twitch_clips
 
-def test_load_a_category():
-    twitch_clips.TwitchClips()
+
+
